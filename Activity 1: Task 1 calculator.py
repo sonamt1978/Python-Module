6ddc24fe5f39157e95266972b8c844fc3376a1cb
sonamt1978 <115384431+sonamt1978@users.noreply.github.com>
2023-01-24 23:55:34 +0600

    Create Activity 1: Task 1 calculator.py

---
+++ Activity 1: Task 1 calculator.py
@@ -0,0 +1,12 @@
+from basic_operation import *
+result = add(5, 3)
+print(result)
+
+result = subtract(5, 3)
+print(result)
+
+result = multiply(5, 3)
+print(result)
+
+result = divide(5, 3)
+print(result)
