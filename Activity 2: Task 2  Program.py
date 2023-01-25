8b6c6f8f86ec6ad29b13847841af3e6672989811
sonamt1978 <115384431+sonamt1978@users.noreply.github.com>
2023-01-25 07:46:02 +0600

    Update Activity 2: Task 2  Program.py

--- Activity 2: Task 2  Program.py
+++ Activity 2: Task 2  Program.py
@@ -1,1 +1,8 @@
+from banking import *
+calc = SavingAccount()
+calc.deposit(1000)
+print(calc.check_balance())
 
+fixed = FixedDeposit()
+fixed.open_fixed_deposit(2000)
+print(fixed.calculate_maturity_amount())
